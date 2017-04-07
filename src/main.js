694dce42f97d32915c01ee237752561ba2de52ea
Daniel West <sqiddster@gmail.com>
2017-04-07 15:45:39 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict";
+import * as EmailValidator from "email-validator";
+console.log(EmailValidator.validate("test"));
+console.log(EmailValidator.validate("test@test.com"));
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+"use strict";
+
+import * as EmailValidator from "email-validator";
+
+console.log(EmailValidator.validate("test"));
+console.log(EmailValidator.validate("test@test.com"));
